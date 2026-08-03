2b58faac9d4d4e039431673729422862eb672ae4
2026aim1020 <2026aim1020@iitrpr.ac.in>
2026-08-03 19:19:21 +0530

    adding bash file for gemm

---
+++ assignment_1/build.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+cd "$(dirname "$0")"
+g++ -O2 -std=c++17 -static-libgcc -static-libstdc++ -Iinclude src/gemm.cpp src/gemm_driver.cpp -o gemm_driver
+echo "Build complete: ./gemm_driver, ./csr_driver"
