1e34910177c6f15b79be6bc72fe947613041bfc3
2026aim1020 <2026aim1020@iitrpr.ac.in>
2026-08-04 15:45:01 +0530

    updated bash

--- assignment_1/build.bash
+++ assignment_1/build.bash
@@ -2,4 +2,5 @@
 set -e
 cd "$(dirname "$0")"
 g++ -O2 -std=c++17 -static-libgcc -static-libstdc++ -Iinclude src/gemm.cpp src/gemm_driver.cpp -o gemm_driver
+g++ -O2 -std=c++17 -static-libgcc -static-libstdc++ -Iinclude src/csr.cpp src/csr_driver.cpp -o csr_driver
 echo "Build complete: ./gemm_driver, ./csr_driver"
